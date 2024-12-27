ac96397306f74a1db90e468f98a51381b8c62289
amitaizenman <amit.aizenman1@mail.huji.ac.il>
2024-12-27 16:35:14 +0200

    added up and down buttons

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] private Button downButton;
+    [SerializeField] private Button upButton;
+}
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,55 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Button upButton;
+    [SerializeField] private Button downButton;
+    [SerializeField] private float moveSpeed = 3f;
+    private string _activeButton = "up";
+
+    private void Awake()
+    {
+        upButton.gameObject.SetActive(true);
+        downButton.gameObject.SetActive(false);
+        
+    }
+
+    private void OnEnable()
+    {
+        upButton.onClick.AddListener(UpButtonClicked);
+        downButton.onClick.AddListener(DownButtonClicked);
+    }
+
+    private void OnDisable()
+    {
+        upButton.onClick.RemoveListener(UpButtonClicked);
+        downButton.onClick.RemoveListener(DownButtonClicked);
+    }
+
+    private void UpButtonClicked()
+    {
+        upButton.gameObject.SetActive(false);
+        transform.DOMove(new Vector3(0.57f,33.6f, -10), moveSpeed);
+        _activeButton = "down";
+        Invoke("ActivateButton", moveSpeed);
+    }
+
+    private void DownButtonClicked()
+    {
+        transform.DOMove(new Vector3(0.57f, -6.68f, -10), moveSpeed);
+        downButton.gameObject.SetActive(false);
+        _activeButton = "up";
+        Invoke("ActivateButton", moveSpeed);
+    }
+
+    private void ActivateButton()
+    {
+        if (_activeButton == "up")
+            upButton.gameObject.SetActive(true);
+        else
+            downButton.gameObject.SetActive(true);
+    }
+}
