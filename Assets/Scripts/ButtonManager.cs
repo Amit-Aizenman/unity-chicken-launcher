25adcd8239e7a22c700e7ab8ba35c1a1792ec896
amitaizenman <amit.aizenman1@mail.huji.ac.il>
2024-12-28 15:42:45 +0200

    changed text

--- Assets/Scripts/ButtonManager.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-
-public class ButtonManager : MonoBehaviour
-{
-    [SerializeField] private Button downButton;
-    [SerializeField] private Button upButton;
-}
